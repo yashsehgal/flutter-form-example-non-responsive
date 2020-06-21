03564beaeda313cf5d966895f2a29737d5185acd
Yash Sehgal <yashsehgal.work@gmail.com>
2020-06-22 01:44:46 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';  
+  
+void main() => runApp(MyApp());  
+  
+class MyApp extends StatelessWidget {  
+  @override  
+  Widget build(BuildContext context) {  
+    final appTitle = 'Flutter Form Demo';  
+    return MaterialApp(  
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      title: appTitle,  
+      home: Scaffold(  
+        appBar: AppBar(  
+          title: Text(appTitle),  
+        ),  
+        body: MyCustomForm(),  
+      ),  
+    );  
+  }  
+}  
+// Create a Form widget.  
+class MyCustomForm extends StatefulWidget {  
+  @override  
+  MyCustomFormState createState() {  
+    return MyCustomFormState();  
+  }  
+}  
+// Create a corresponding State class. This class holds data related to the form.  
+class MyCustomFormState extends State<MyCustomForm> {  
+  // Create a global key that uniquely identifies the Form widget  
+  // and allows validation of the form.  
+  final _formKey = GlobalKey<FormState>();  
+  
+  @override  
+  Widget build(BuildContext context) {  
+    // Build a Form widget using the _formKey created above.  
+    return Form(  
+      key: _formKey,  
+      child: Column(  
+        crossAxisAlignment: CrossAxisAlignment.start,  
+        children: <Widget>[  
+          TextFormField(  
+            decoration: const InputDecoration(  
+              icon: const Icon(Icons.person),  
+              hintText: 'Enter your name',  
+              labelText: 'Name',  
+            ),  
+          ),  
+          TextFormField(  
+            decoration: const InputDecoration(  
+              icon: const Icon(Icons.phone),  
+              hintText: 'Enter a phone number',  
+              labelText: 'Phone',  
+            ),  
+          ),  
+          TextFormField(  
+            decoration: const InputDecoration(  
+            icon: const Icon(Icons.calendar_today),  
+            hintText: 'Enter your date of birth',  
+            labelText: 'Dob',  
+            ),  
+           ),  
+          new Container(  
+              padding: const EdgeInsets.only(left: 150.0, top: 40.0),  
+              child: new RaisedButton(  
+                child: const Text('Submit'),  
+                  onPressed: null,  
+              )),  
+        ],  
+      ),  
+    );  
+  }  
+}  
